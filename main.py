47d5010667c8fd43d0f698b49f40dd5ee211706c
Eric Nelson <Nelson@Erics-MacBook-Pro.local>
2016-09-19 14:26:52 -0500

    Final Commit

--- main.py
+++ main.py
@@ -1,25 +1,89 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Blog(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("/blog")
+
+
+class MainPage(Handler):
+    def render_front(self, subject="", content="", error="", article=""):
+        article = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5 OFFSET 0")
+        self.render("front.html", subject=subject, content=content, error=error, article=article)
+
+    def get(self):
+        self.render_front()
+        #self.render("newpost.html")
+
+class NewPost(Handler):
+    def get(self):
+        self.render("newpost.html")
+        #self.response.write(response)
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            a = Blog(subject = subject, content = content)
+            a.put()
+
+            number=a.key().id()
+
+            #redirect sends browser to the url in the parenthesis
+            string="/blog/{0}".format(number)
+
+            self.redirect(string)
+
+        else:
+            error = "we need both subject and content"
+            self.render("newpost.html", subject=subject, content=content, error=error)
+
+class ViewPostHandler(Handler):
+    def get(self, id):
+
+        post = Blog.get_by_id(int(id))
+
+        if not post:
+            self.error(404)
+            return
+        self.render("post.html", post=post)
+        #self.render("permalink.html", post=post)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    ('/', MainHandler),
+    ('/blog', MainPage),
+    ('/blog/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
+    #('/blog/postpage', PostPage),
+    #('/blog/<id:\d+>', ViewPostHandler)
+    ], debug=True)
+     #webapp2.Route('/blog/<id:\d+>', ViewPostHandler)#('/blog/?', BlogFront),
